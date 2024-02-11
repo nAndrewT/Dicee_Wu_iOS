1932904f1319d9ce950f286c772c4f5e7b74cd77
Noah T <noahtofte@gmail.com>
2024-02-11 17:09:26 -0500

    frontend and randomization complete

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDice: Int = 0
+    var rightDice: Int = 0
+    var score: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPress(_ sender: UIButton) {
+        
+        let dices = [UIImage(imageLiteralResourceName: "DiceOne"),
+                     UIImage(imageLiteralResourceName: "DiceTwo"),
+                     UIImage(imageLiteralResourceName: "DiceThree"),
+                     UIImage(imageLiteralResourceName: "DiceFour"),
+                     UIImage(imageLiteralResourceName: "DiceFive"),
+                     UIImage(imageLiteralResourceName: "DiceSix")
+                    ]
+        
+        leftDice = Int.random(in: 0...5)
+        rightDice = Int.random(in: 0...5)
+        
+        score = leftDice + rightDice + 2
+        
+        diceImageView1.image = dices[leftDice]
+        diceImageView2.image = dices[rightDice]
+        
+        print(score)
+    }
+    
 }
 
